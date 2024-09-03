dab4e1db394c7c8eb63f10a7000bd11e9111418d
superLionn <1995junwei@gmail.com>
2024-09-03 10:12:35 +0000

    add yml file

--- obj/Debug/net8.0/MyFirstAzureWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyFirstAzureWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acdfac66247a9ef031c9187bd993c9020b770c35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92cbba410bd09d72961e9211296d7316c95e1eb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
